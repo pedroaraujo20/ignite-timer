138ab5478d4b5ed0237551738ce56d9c6e2db821
Pedro Araujo <paraujo@avantstay.com>
2023-05-21 17:12:20 -0300

    feat: create ignite timer structure

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,30 @@
+import styled, { css } from 'styled-components'
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
+
+interface ButtonContainerProps {
+  variant: ButtonVariant
+}
+
+const buttonVariants = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  color: ${({ theme }) => theme.white};
+
+  background-color: ${({ theme }) => theme['green-500']};
+
+  /* ${({ variant }) =>
+    css`
+      background-color: ${buttonVariants[variant]};
+    `} */
+`
